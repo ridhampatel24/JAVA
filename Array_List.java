2009e996c05279c1bb715158332296a907749c6d
Ridham Patel <105585070+ridhampatel24@users.noreply.github.com>
2022-09-25 19:04:12 +0530

    Create Array_List.java

---
+++ Array_List.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class Array_List {
+
+    public static int arrlist_max(ArrayList<Integer> list) {
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < list.size(); i++) {
+            max = Math.max(max, list.get(i));
+        }
+        return max;
+    }
+
+    public static void swap(ArrayList<Integer> list, int idx1, int idx2) {
+        int temp = list.get(idx1);
+        list.set(idx1, list.get(idx2));
+        list.set(idx2, temp);
+    }
+    public static void main(String[] args) {
+        
+        ArrayList <Integer> list = new ArrayList<>();
+        list.add(2);
+        list.add(5);
+        list.add(9);
+        list.add(6);
+        list.add(8);
+
+        System.out.println(list);
+        Collections.sort(list); // ascending
+        System.out.println(list);
+        Collections.sort(list, Collections.reverseOrder());  ///descending
+        System.out.println(list);
+
+        
+
+
+    }
+}
